d44060be224b717a51df5993a9c00a8655ba8510
Max Semionov <semenov.max.art@mail.ru>
2026-09-14 16:52:00 +0300

    move sorting to logic

--- logic.py
+++ logic.py
@@ -4,7 +4,7 @@ def getRegions(rows, regionId):
         region = row[regionId]
         if region not in regions:
             regions.append(region)
-    return regions
+    return sorted(regions)
 
 
 def selectRegion(rows, regionId, region):
